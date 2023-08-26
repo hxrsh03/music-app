6dfb3e756a91f082129a362ac7a311ceee244382
hxrsh03 <140371829+hxrsh03@users.noreply.github.com>
2023-08-26 13:05:15 +0530

    Add files via upload

---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,37 @@
+import {Search} from "../components/Search"
+import {Songs} from "../components/Songs"
+import {getSongs} from "../services/api-client"
+import {useEffect,useState} from "react";
+import {Player} from "../components/Player"
+export const SearchPage =()=>{
+    const [allSongs,setSongs] = useState([]);
+    const [flag,setFlag]= useState(false);
+    const [song,setPlayerSong] =useState(null);
+    const loadSongs=async()=>{
+        setSongs(await getSongs('Latest Songs'));
+    }
+    useEffect(()=>{
+        loadSongs();
+    },[])
+
+
+    const togglePlayer=(flag,songArg)=>{
+        setPlayerSong(songArg);
+        setFlag(flag);
+
+    }
+    const getArtistName = async (artistName)=>{
+       
+        console.log("rec artist name",artistName)
+       setSongs(await getSongs(artistName));
+
+    }
+    const jsx=<> <Search fn={getArtistName}/>
+    <Songs fn={togglePlayer} allSongs={allSongs}/></>;
+    return(<div className="container">
+        <h1 className="alert alert-primary text-center">MUSIC PLAYER</h1>
+       
+        {flag?<Player fn={togglePlayer} song={song}/>:jsx}
+       
+    </div>)
+}   
